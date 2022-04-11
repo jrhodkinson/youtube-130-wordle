491d4357f475db6d1dd3160567b588cd652564bf
Jack Hodkinson <jrh206@cam.ac.uk>
2022-04-11 15:14:04 +0100

    Improve title

--- src/main/java/com/youtube/wordle/WordleProperties.java
+++ src/main/java/com/youtube/wordle/WordleProperties.java
@@ -36,7 +36,7 @@ public class WordleProperties
 
     public String title(Word word)
     {
-        return "This video can solve today's Wordle (seriously. it's %s)".formatted(word);
+        return "This video solved today's Wordle (seriously. it's %s)".formatted(word);
     }
 
     public String wordleUrl()
